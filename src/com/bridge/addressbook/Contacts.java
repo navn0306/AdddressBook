86463efbd88985b52b96e69389888837fcd12521
Navneet <navn0306@gmail.com>
2021-12-08 16:59:51 +0530

    ADD: Feature to add multiple contacts in addressbook using arraylist

--- src/com/bridge/addressbook/Contacts.java
+++ src/com/bridge/addressbook/Contacts.java
@@ -7,27 +7,38 @@ public class Contacts {
 
         Collection<AddressBook> a = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
+        Scanner c = new Scanner(System.in);
+        int ch;
+        System.out.println("To enter a record press 1 or 0 to exit");
+        ch = c.nextInt();
+        while (ch != 0) {
 
-        System.out.println("Enter Fname - ");
-        String fname = sc.nextLine();
-        System.out.println("Enter lname - ");
-        String lname = sc.nextLine();
-        System.out.println("Enter address - ");
-        String address = sc.nextLine();
-        System.out.println("Enter city - ");
-        String city = sc.nextLine();
-        System.out.println("Enter state - ");
-        String state = sc.nextLine();
-        System.out.println("Enter email - ");
-        String email = sc.nextLine();
-        System.out.println("Enter number - ");
-        String number = sc.nextLine();
-        System.out.println("Enter zip - ");
-        String zip = sc.nextLine();
+            System.out.println("Enter Fname - ");
+            String fname = sc.nextLine();
+            System.out.println("Enter lname - ");
+            String lname = sc.nextLine();
+            System.out.println("Enter address - ");
+            String address = sc.nextLine();
+            System.out.println("Enter city - ");
+            String city = sc.nextLine();
+            System.out.println("Enter state - ");
+            String state = sc.nextLine();
+            System.out.println("Enter email - ");
+            String email = sc.nextLine();
+            System.out.println("Enter number - ");
+            String number = sc.nextLine();
+            System.out.println("Enter zip - ");
+            String zip = sc.nextLine();
 
-        a.add(new AddressBook(fname, lname, address, city, state, email, number, zip));
-        System.out.println(" Details ");
-        System.out.println(a);
-
+            a.add(new AddressBook(fname, lname, address, city, state, email, number, zip));
+            System.out.println("To enter a record press 1or 0 to exit");
+            ch = c.nextInt();
+        }
+        System.out.println(" Contacts ");
+        Iterator<AddressBook> i =a.iterator();
+        while (i.hasNext()){
+            AddressBook address = i.next();
+            System.out.println(address);
+        }
     }
 }

d3dcd33dac1b86594c53e158c2719786d3b9aca5
Navneet <navn0306@gmail.com>
2021-12-29 18:07:28 +0530

    ADD: Feature to search a contact by cityname.

--- src/com/bridge/addressbook/Contacts.java
+++ src/com/bridge/addressbook/Contacts.java
@@ -1,51 +1,45 @@
 package com.bridge.addressbook;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Contacts {
 
-    public static ArrayList<Operations> contactBookOptions() {
-
-        Operations oprations = new Operations();
-
-        ArrayList<Operations> operations = new ArrayList<>();
+    public static ArrayList<Operations> addressBook() {
+        ArrayList<Operations> contact = new ArrayList<>();
+        Operations operations = new Operations();
         String name;
         Scanner sc = new Scanner(System.in);
         int ch = 1;
-
-
         while (ch != 0) {
-            System.out.print(" To Add contact enter 1 \nTo Edit contact enter 2 \nTo Delete contact enter 1 \nTo display contact enter 4");
+            System.out.print(" To Add contact enter 1 \nTo Edit contact enter 2 \nTo Delete contact enter 1 " +
+                    "\nTo display contact enter 4\nTo exit enter 0");
             ch = sc.nextInt();
-
             switch (ch) {
                 case 1:
-                    Operations operations1 = Operations.read(operations);
-                    if (operations1.fName != null) {
-                        operations.add(operations1);
+                    Operations operations1 = operations.read();
+                    if (contact != null) {
+                        contact.add(operations1);
                     }
                     break;
                 case 2:
                     System.out.println("Enter first name that you want to edit contactDetails");
                     name = sc.next();
-                    oprations.updateData(name, operations);
+                    Operations.update(name, contact);
                     break;
                 case 3:
                     System.out.println("Enter the first name that you want to delete Contact details ");
                     name = sc.next();
-                    oprations.deleteRecord(name, operations);
+                    Operations.delete(name, contact);
                     break;
                 case 4:
-                    System.out.println("contactDetails : " + operations.size());
-                    oprations.addressBook(operations);
-                    break;
-                default:
-                    System.out.println("Enter correct option");
+                    System.out.println("ContactDetails: " + contact.size());
+                    Operations.book(contact);
                     break;
 
+                case 0:
+                    break;
             }
         }
-        return operations;
+        return contact;
     }
 }
--- src/com/bridge/addressbook/Operations.java
+++ src/com/bridge/addressbook/Operations.java
@@ -4,12 +4,9 @@ import java.util.*;
 
 public class Operations {
     String fName, lName, city, state, address, email, phoneNo, zip;
-    int count = 1;
 
-    public void Operations() {
-    }
-
-    public void addressBook(ArrayList<Operations> contactPerson) {
+    public static void book(ArrayList<Operations> contactPerson) {
+        int count = 1;
         for (int i = 0; i < contactPerson.size(); i++) {
             System.out.println("contact" + count);
             System.out.println("First Name: " + contactPerson.get(i).fName);
@@ -24,26 +21,24 @@ public class Operations {
         }
     }
 
-    public void updateData(String name, ArrayList<Operations> contactDetails) {
+    public static void update(String name, ArrayList<Operations> contactDetails) {
         for (int i = 0; i < contactDetails.size(); i++) {
             if (name.equals(contactDetails.get(i).fName)) {
                 contactDetails.remove(i);
-                contactDetails.add(i, read(contactDetails));
+                contactDetails.add(i, read());
             }
         }
         System.out.println("Record Update Successfully");
     }
 
-    public static Operations read(ArrayList<Operations> operations) {
+//To read the data from console
+    public static Operations read() {
 
         Operations contactPersonDetails = new Operations();
         Scanner sc = new Scanner(System.in);
         System.out.print("First Name : ");
         contactPersonDetails.fName = sc.next();
-        if (operations.stream().filter(name -> name.fName.equals(contactPersonDetails.fName)).count() > 0) {
-            System.out.println("Contact already exists");
-            return new Operations();
-        }
+
         System.out.print("Last Name : ");
         contactPersonDetails.lName = sc.next();
 
@@ -64,7 +59,8 @@ public class Operations {
         return contactPersonDetails;
     }
 
-    public void deleteRecord(String name, ArrayList<Operations> contactDetails) {
+//To delete the entered contact
+    public static void delete(String name, ArrayList<Operations> contactDetails) {
         if (contactDetails.size() > 0) {
             for (int i = 0; i < contactDetails.size(); i++) {
                 if (name.equals(contactDetails.get(i).fName)) {
@@ -75,4 +71,31 @@ public class Operations {
         System.out.println("Record Delete Successfully");
     }
 
+//To search contact using city name
+    public static void search(String city, Map<String, ArrayList<Operations>> map) {
+        map
+                .values()
+                .stream().
+                forEach(value -> {
+                    value.stream()
+                            .forEach(person -> {
+                                if (person.city.contains(city) || person.state.contains(city))
+                                    System.out.println(person);
+                            });
+                });
+    }
+
+    @Override
+    public String toString() {
+        return "Operations{" +
+                "fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
 }
